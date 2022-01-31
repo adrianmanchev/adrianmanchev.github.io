7645bc463c98fa859a5d3da62082c70b412634d4
Ali Öztürk <ax.ozturk@gmail.com>
2022-01-31 23:27:19 +0300

    trading-charts

---
+++ trading-charts/precache-manifest.0f50adac0f132a5824beb9b457c956df.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23fce2bc0f22e3f7bf80",
+    "url": "/trading-charts/css/app.384d2efa.css"
+  },
+  {
+    "revision": "72cc680224bfd1aabb3c",
+    "url": "/trading-charts/css/currencies-detail.a2399577.css"
+  },
+  {
+    "revision": "7321a5c4e2a5df026a063e326b36bfe9",
+    "url": "/trading-charts/img/bootstrap.7321a5c4.png"
+  },
+  {
+    "revision": "ffc6dddb7a4829aed67d19c082d5fc16",
+    "url": "/trading-charts/img/d3.ffc6dddb.svg"
+  },
+  {
+    "revision": "d95a6b11bcc4f6790702e8dbec92d0c7",
+    "url": "/trading-charts/img/phone.d95a6b11.svg"
+  },
+  {
+    "revision": "0a594dd6da33e6ad05c43efa53a19937",
+    "url": "/trading-charts/img/price-list.0a594dd6.jpeg"
+  },
+  {
+    "revision": "346e12ee28bb0e5f5600d47beb4c7a47",
+    "url": "/trading-charts/img/vue.346e12ee.svg"
+  },
+  {
+    "revision": "3e65df6aa02a0aba3e53ec1770af886e",
+    "url": "/trading-charts/img/websocket.3e65df6a.svg"
+  },
+  {
+    "revision": "d3aecef36bfe7a9ffb8188513023019d",
+    "url": "/trading-charts/index.html"
+  },
+  {
+    "revision": "23fce2bc0f22e3f7bf80",
+    "url": "/trading-charts/js/app.cd265c28.js"
+  },
+  {
+    "revision": "79ef0011afcfb17cb0d3",
+    "url": "/trading-charts/js/chunk-vendors.f3ddc0bd.js"
+  },
+  {
+    "revision": "72cc680224bfd1aabb3c",
+    "url": "/trading-charts/js/currencies-detail.e90888b3.js"
+  },
+  {
+    "revision": "6f7f8292f21d30da7974",
+    "url": "/trading-charts/js/currencies-top.563ea9cc.js"
+  },
+  {
+    "revision": "87a3b649ba201b54e916",
+    "url": "/trading-charts/js/currencies.bc03333d.js"
+  },
+  {
+    "revision": "bcaa3501647f87a3cd6d",
+    "url": "/trading-charts/js/not-found.aa7c281c.js"
+  },
+  {
+    "revision": "118a2d8ca17248d24063",
+    "url": "/trading-charts/js/shortcodes.471f6cc6.js"
+  },
+  {
+    "revision": "aa01cda7389691a3d02e3d9ca1d3a083",
+    "url": "/trading-charts/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/trading-charts/robots.txt"
+  }
+]);
